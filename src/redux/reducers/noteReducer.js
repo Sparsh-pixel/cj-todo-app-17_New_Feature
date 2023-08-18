f859499bf602e0c50736a62225aaebaa0ddc0650
Sparsh-pixel <sparshprasad7@gmail.com>
2023-08-18 12:53:05 +0530

    using redux_toolkit

--- src/redux/reducers/noteReducer.js
+++ src/redux/reducers/noteReducer.js
@@ -1,32 +1,58 @@
-import {ADD_NOTES,DELETE_NOTES} from "../actions/noteActions";
+// import {ADD_NOTES,DELETE_NOTES} from "../actions/noteActions";
+
+const { createSlice } = require("@reduxjs/toolkit")
 
 const initialState = {
     notes:[{text: " hello there i am new to redux and and react", createdOn: new Date()},
 {text: "new features are adding on the todo app. it is little complicated", createdOn: new Date()}]
 }
 
-export function noteReducer (state= initialState, action) {
-    switch (action.type) {
-        case ADD_NOTES:
-            return {
-                ...state,
-                notes:[
-                    ...state.notes,
-                    {
-                        text:action.text,
-                        createdOn: new Date(),
-                    }
-                ]
 
-            }
-            case DELETE_NOTES:
-                state.notes.splice(action.index,1);
-                return {
-                    ...state,
-                    notes: [...state.notes]
-                }
-                default: 
-                return state;
+const noteSlice = createSlice ({
+    name : "note",
+    initialState : initialState,
+    reducers: {
+        // this is add reducers
+        add: (state,action) => {
+            state.notes.push[{
+                text : action.payload,
+                created: new Date(),
+
+            }]
+        },
+        // this is delete reducers
+        delete: (state,action) => {
+            state.notes.splice(action.payload,1);
+        },
+
     }
+}); 
 
-}
+
+// this is example of reducer function using the redux function
+
+// export function noteReducer (state= initialState, action) {
+//     switch (action.type) {
+//         case ADD_NOTES:
+//             return {
+//                 ...state,
+//                 notes:[
+//                     ...state.notes,
+//                     {
+//                         text:action.text,
+//                         createdOn: new Date(),
+//                     }
+//                 ]
+
+//             }
+//             case DELETE_NOTES:
+//                 state.notes.splice(action.index,1);
+//                 return {
+//                     ...state,
+//                     notes: [...state.notes]
+//                 }
+//                 default: 
+//                 return state;
+//     }
+
+// }
--- src/redux/reducers/todoReducer.js
+++ src/redux/reducers/todoReducer.js
@@ -1,5 +1,7 @@
 
-import { ADD_TODO, TOGGLE_TODO } from "../actions/todoActions";
+// import { ADD_TODO, TOGGLE_TODO } from "../actions/todoActions";
+
+const { createSlice } = require("@reduxjs/toolkit")
 
 const initialState={
     todos:[
@@ -8,31 +10,72 @@ const initialState={
     ]
 }
 
-export function todoReducer(state=initialState, action){
+//  creating reducer using redux-toolkit
 
-    switch(action.type){
-        case ADD_TODO:
-            return {
-                ...state,
-                todos:[
-                    ...state.todos,
-                    {
-                        text:action.text,
-                        completed: false
-                    }
-                ]
-            }
-        case TOGGLE_TODO:
-            return{
-                ...state,
-                todos: state.todos.map((todo, i)=>{
-                    if(i===action.index){
-                        todo.completed=!todo.completed
-                    }
-                    return todo;
-                })
-            }
-        default:
-            return state;
+const todoSlice = createSlice({
+    // this is are the properties of the createSlice function
+    name: "todo",
+    initialState: initialState,
+    reducers: {
+        // this is add actions
+        add: (state,action) => {
+            state.todos.push[{
+                text:action.payload,
+                completed:false
+            }]
+
+        },
+//  this is toggle action
+        toggle: (state,action) => {
+            state.todos.map((todo,i)=>{
+                if(i===action.payload){
+                    todo.completed =!todo.completed
+                }
+                return todo;
+
+            })
+        },
+
     }
-}
+})
+
+
+
+
+
+
+
+
+
+
+
+// below is the example of a reducer using redux
+
+// export function todoReducer(state=initialState, action){
+
+//     switch(action.type){
+//         case ADD_TODO:
+//             return {
+//                 ...state,
+//                 todos:[
+//                     ...state.todos,
+//                     {
+//                         text:action.text,
+//                         completed: false
+//                     }
+//                 ]
+//             }
+//         case TOGGLE_TODO:
+//             return{
+//                 ...state,
+//                 todos: state.todos.map((todo, i)=>{
+//                     if(i===action.index){
+//                         todo.completed=!todo.completed
+//                     }
+//                     return todo;
+//                 })
+//             }
+//         default:
+//             return state;
+//     }
+// }
